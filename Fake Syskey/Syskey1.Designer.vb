cc88499e47bf901362b6d0da7d593d37c73c63f1
WindowsLogic <admin@windowslogic.co.uk>
2019-03-24 16:33:57 +0000

    Added original project files and changed directory

---
+++ Fake Syskey/Syskey1.Designer.vb
@@ -0,0 +1,164 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Syskey1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Syskey1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.btnok = New System.Windows.Forms.Button()
+        Me.btncancel = New System.Windows.Forms.Button()
+        Me.btnupd = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(-334, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(248, 54)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "This tool will allow you to configure the Accounts Database to enable additional " &
+    "encryption, further protecting the database from compromise"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(83, 17)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(245, 42)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "This tool will allow you to configure the Accounts Database to enable addition en" &
+    "cryption, further protecting the database from compromise."
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Fake_Syskey.My.Resources.Resources.Syskey_Picture
+        Me.PictureBox1.Location = New System.Drawing.Point(20, 17)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(50, 37)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.Location = New System.Drawing.Point(83, 74)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(245, 27)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Once enabled, This encryption cannot be disabled."
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Enabled = False
+        Me.RadioButton1.Location = New System.Drawing.Point(86, 114)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(119, 17)
+        Me.RadioButton1.TabIndex = 4
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Encryption Disabled"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(86, 137)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(112, 17)
+        Me.RadioButton2.TabIndex = 5
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Encrption Enabled"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'btnok
+        '
+        Me.btnok.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.btnok.Location = New System.Drawing.Point(54, 169)
+        Me.btnok.Name = "btnok"
+        Me.btnok.Size = New System.Drawing.Size(75, 23)
+        Me.btnok.TabIndex = 6
+        Me.btnok.Text = "OK"
+        Me.btnok.UseVisualStyleBackColor = True
+        '
+        'btncancel
+        '
+        Me.btncancel.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.btncancel.Location = New System.Drawing.Point(135, 169)
+        Me.btncancel.Name = "btncancel"
+        Me.btncancel.Size = New System.Drawing.Size(75, 23)
+        Me.btncancel.TabIndex = 7
+        Me.btncancel.Text = "Cancel"
+        Me.btncancel.UseVisualStyleBackColor = True
+        '
+        'btnupd
+        '
+        Me.btnupd.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.btnupd.Location = New System.Drawing.Point(216, 169)
+        Me.btnupd.Name = "btnupd"
+        Me.btnupd.Size = New System.Drawing.Size(75, 23)
+        Me.btnupd.TabIndex = 8
+        Me.btnupd.Text = "Update"
+        Me.btnupd.UseVisualStyleBackColor = True
+        '
+        'Syskey1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(340, 198)
+        Me.Controls.Add(Me.btnupd)
+        Me.Controls.Add(Me.btncancel)
+        Me.Controls.Add(Me.btnok)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Syskey1"
+        Me.ShowIcon = False
+        Me.Text = "Securing the Windows Account Database"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents btnok As System.Windows.Forms.Button
+    Friend WithEvents btncancel As System.Windows.Forms.Button
+    Friend WithEvents btnupd As System.Windows.Forms.Button
+
+End Class
